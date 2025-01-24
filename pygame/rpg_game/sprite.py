dd1cbcd766e91561208e504e09e043fd61f24c7a
yehiyan <alexyeh0725@gmail.com>
2025-01-24 09:20:17 +0800

    modified:   sprite.py

--- pygame/rpg_game/sprite.py
+++ pygame/rpg_game/sprite.py
@@ -855,7 +855,7 @@ class Pokemon(pygame.sprite.Sprite):
 
     def perform_attack(self, other, move, power):
 
-        display_message(self.game, f'{self.name} ä½¿å?? {move}')
+        display_message(self.game, f'{self.name} ¨Ï¥X {move}')
 
         #pause for 2 seconds
         time.sleep(2)
